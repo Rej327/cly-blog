35ee2ac324a1fd33329111ad20c5a8cf79c60c4f
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-12-01 10:34:33 +0800

    Update

--- src/components/MostPopularPost.js
+++ src/components/MostPopularPost.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {
   fashionist,
-  visits,
+  places,
   slide1,
   slide2,
   slide3,
@@ -21,8 +21,12 @@ const MostPopularPost = () => {
         </div>
         <div className="grid grid-cols-3 mx-auto w-full h-full gap-8 px-4">
           <div className=" rounded-md bg-white h-[35rem] w-full">
-            <img src={fashionist} alt="/" />
-            <div className="px-5 py-10 text-center">
+            <img
+              src={fashionist}
+              alt="/"
+              className="rounded-t-md hover:scale-105 duration-500"
+            />
+            <div className="px-5 py-8 text-center">
               <h1 className="font-tenor text-2xl">
                 How to become a fashionist?
               </h1>
@@ -33,12 +37,29 @@ const MostPopularPost = () => {
                 Separated they live in Bookmarksgrove right at the coast of the
                 Semantics, a large language ocean.
               </p>
-              <button class="bg-[#E2B8AC] hover:bg-[#e0bbb0] text-white font-bold my-4 py-2 px-4 rounded-full">
+              <button class="bg-[#E2B8AC] hover:bg-[#e0bbb0] text-white  my-5 py-2 px-4 rounded-full hover:drop-shadow-md duration-500 hover:scale-105">
                 Read More
               </button>
             </div>
           </div>
-          <div className="bg-red-600 h-[45rem] w-full"></div>
+          <div className=" rounded-md bg-white h-[48rem] w-full">
+            <img src={places} alt="/" className="rounded-t-md" />
+            <div className="px-5 py-8 text-center">
+              <h1 className="font-tenor text-2xl">
+                30 Places you want to visits
+              </h1>
+              <p className="text-[12px] text-stone-400 tracking-widest my-5">
+                Feb 07, 2018 | Web Design
+              </p>
+              <p className="text-stone-500">
+                Separated they live in Bookmarksgrove right at the coast of the
+                Semantics, a large language ocean.
+              </p>
+              <button class="bg-[#E2B8AC] hover:bg-[#e0bbb0] text-white  my-5 py-2 px-4 rounded-full hover:drop-shadow-md duration-500 hover:scale-105">
+                Read More
+              </button>
+            </div>
+          </div>
           <div className="bg-red-600 h-[35rem] w-full"></div>
           <div className="bg-red-600 h-[35rem] my-[-10rem] w-full"></div>
           <div className="bg-red-600 h-[45rem] w-full"></div>
---
+++ src/components/SlidePost.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation, Autoplay } from "swiper";
+import { slide1, slide2, slide3 } from "../assets";
+
+export default function slidepost() {
+  return (
+    <div className="h-full w-full">
+      <Swiper
+        slidesPerView={1}
+        slidesPerGroup={1}
+        loop={true}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation, Autoplay]}
+        className="h-full"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="/" className="object-cover h-full" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="/" className="object-cover h-full" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="/" className="object-cover h-full" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
