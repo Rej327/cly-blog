6855c21d2572e32437c074fb8253af5054445016
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-12-05 12:30:27 +0800

    Update

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,19 +2,26 @@ import React from "react";
 
 const Newsletter = () => {
   return (
-    <div>
+    <div className="w-auto">
       <div>
-        <h1>NEWSLETTER</h1>
-        <p>
+        <h1 className="font-tenor text-xl py-5 tracking-wider">NEWSLETTER</h1>
+        <p className="text-gray-600">
           A small river named Duden flows by their place and supplies it with
           the necessary regelialia
         </p>
       </div>
       <div>
-        <div>
-          <input type="email" name="email" placeholder="Type your email" />
-          <button>Subscribe</button>
+        <div className="py-3 w-full">
+          <input
+            type="email"
+            name="email"
+            placeholder="Type your email"
+            className="w-full h-11 text-center rounded-md outline-[#E2B8AC]"
+          />
         </div>
+        <button className="bg-[#E2B8AC] hover:bg-[#e0bbb0] text-white py-2 px-4 rounded-full hover:drop-shadow-md duration-500 hover:scale-105 w-full">
+          Subscribe
+        </button>
       </div>
     </div>
   );
