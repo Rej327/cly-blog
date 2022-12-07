288c887c55e407db62be8445a6464a0a8dca3c04
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-12-07 11:07:43 +0800

    add blog layout

--- src/components/AddBlog.js
+++ src/components/AddBlog.js
@@ -1,17 +1,28 @@
 import React from "react";
+import { VscAdd } from "react-icons/vsc";
 
 const AddBlog = () => {
   return (
     <div className="max-full h-screen bg-[#E2B8AC]/50 flex justify-center">
       <div className="m-auto bg-white text-center w-96 h-96 inline-block rounded-md p-4">
-        <h1 className="pb-4">Add Blog</h1>
+        <h1 className="pb-4 font-tenor tracking-wider text-2xl">ADD BLOG</h1>
         <form className="grid">
-          <label className="font-tenor">TITLE</label>
-          <input className="inline-block" type="text" required />
-          <label>CONTENT</label>
-          <textarea required />
+          <label className="pb-2">TITLE</label>
+          <input
+            className="inline-block p-2 bg-slate-50 rounded-md outline-[#E2B8AC]"
+            type="text"
+            placeholder="Enter you title here..."
+            required
+          />
+          <label className="p-4">CONTENT</label>
+          <textarea
+            className="inline-block max-h-28 p-2 bg-slate-50 rounded-md outline-[#E2B8AC]"
+            type="text"
+            placeholder="Enter you blog here..."
+            required
+          />
         </form>
-        <button>Add</button>
+        <VscAdd className="h-10 p-2 mx-auto cursor-pointer mt-4 w-10 bg-[#E2B8AC] rounded-md hover:text-white hover:scale-105 hover:shadow-md duration-300" />
       </div>
     </div>
   );
