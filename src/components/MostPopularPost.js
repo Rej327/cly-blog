8852b0331a5641ff7f0afc32adf2438764d3aee5
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2022-12-11 12:53:01 +0800

    fixing data with .json

--- src/components/MostPopularPost.js
+++ src/components/MostPopularPost.js
@@ -4,6 +4,7 @@ import { data } from "../data/data";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import "aos/dist/aos.css";
 import Qoutes from "./Qoutes";
+import blogs from "../data/blogs.json";
 
 const MostPopularPost = () => {
   return (
@@ -24,41 +25,41 @@ const MostPopularPost = () => {
             columnsCountBreakPoints={{ 320: 1, 768: 2, 900: 3 }}
           >
             <Masonry gutter="20px">
-              {data.map((data) => (
+              {blogs.map((blogs) => (
                 <div
                   data-aos="fade-up"
                   data-aos-duration="2000"
                   className=" rounded-md bg-white w-full"
                 >
-                  {data.img ? (
+                  {blogs.img ? (
                     <img
-                      src={data.img}
+                      src={blogs.img}
                       alt="/"
                       className="rounded-t-md hover:scale-105 duration-500"
                     />
                   ) : undefined}
 
-                  {data.video ? (
-                    <video src={data.video} autoPlay="true" loop controls />
+                  {blogs.video ? (
+                    <video src={blogs.video} autoPlay="true" loop controls />
                   ) : undefined}
 
-                  {data.component ? <SlidePost /> : undefined}
+                  {blogs.component ? <SlidePost /> : undefined}
 
-                  {data.components ? <Qoutes /> : undefined}
+                  {blogs.components ? <Qoutes /> : undefined}
 
-                  {data.audio ? (
+                  {blogs.audio ? (
                     <audio className="mx-auto mt-5" src={data.audio} controls />
                   ) : undefined}
 
                   <div className="px-5 py-8 text-center">
-                    <h1 className="font-tenor text-2xl">{data.head}</h1>
+                    <h1 className="font-tenor text-2xl">{blogs.head}</h1>
                     <p className="text-[12px] text-stone-400 tracking-widest my-5">
                       Post: {""}
-                      {data.date}
+                      {blogs.date}
                     </p>
-                    <p className="text-stone-500">{data.content}</p>
+                    <p className="text-stone-500">{blogs.content}</p>
                     <button className="bg-[#E2B8AC] hover:bg-[#e0bbb0] text-white  my-5 py-2 px-4 rounded-full hover:drop-shadow-md duration-500 hover:scale-105">
-                      {data.button}
+                      {blogs.button}
                     </button>
                   </div>
                 </div>
