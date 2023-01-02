9f9e06fd0a2b4f72eafcf4d94b55017ff7d7604f
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-01-02 11:15:55 +0800

    Express App Setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require("express");
+
+require("dotenv").config();
+
+// express app
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
+//middleware
+app.use(express.json());
+app.use((res, req, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+// routes
+app.get("/", (req, res) => {
+  res.json({ blog: "My Blog" });
+});
+
+// listen for request
+app.listen(process.env.PORT, () => {
+  console.log("Server started on port", process.env.PORT);
 });
