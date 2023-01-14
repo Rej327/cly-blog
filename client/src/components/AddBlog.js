f6ea304eafbe353e690415f9c6d369afe80d3648
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-01-14 11:21:48 +0800

    Add design in Add blog component

--- client/src/components/AddBlog.js
+++ client/src/components/AddBlog.js
@@ -45,43 +45,51 @@ const AddBlog = () => {
   };
 
   return (
-    <form className="create" onSubmit={handleSubmit}>
+    <form
+      className="mx-auto w-[650px] grid border-dashed border-black-800 border-2 p-6 my-6"
+      onSubmit={handleSubmit}
+    >
+      <h3 className="py-4 text-2xl text-center">Add a New Blog</h3>
+      <label className="py-2 text-xl">Blog Title </label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        className={emptyFields.includes("title") ? "error" : "rounded h-10 p-2"}
+      />
+      <label className="py-2 text-xl">Content </label>
+      <textarea
+        type="text"
+        onChange={(e) => setContent(e.target.value)}
+        value={content}
+        className={
+          emptyFields.includes("content") ? "error" : "rounded h-[150px] p-2"
+        }
+      />
+
+      {/* Add Image */}
       {image ? (
         <img
           src={image && image.filesUploaded[0].url}
           alt="imageUploaded"
           onChange={(e) => setFile(e.target.value)}
           value={file}
-          className="w-[400px] h-[400px] object-cover"
+          className="w-[400px] h-[400px] object-cover mx-auto p-2"
         />
       ) : (
         <button
           type="button"
           onClick={() => (isPicker ? setIsPicker(false) : setIsPicker(true))}
-          className="w-full text-lg font-bold border-dashed h-56 border-4 border-blue-800 text-blue-800"
+          className="w-full text-lg border-dashed h-36 border-4 border-[#E2B8AC] mt-2"
         >
           Add Image
         </button>
       )}
 
-      <h3>Add a New Workout</h3>
-      <label>Blog Title </label>
-      <input
-        type="text"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        className={emptyFields.includes("title") ? "error" : ""}
-      />
-      <label>Content </label>
-      <input
-        type="text"
-        onChange={(e) => setContent(e.target.value)}
-        value={content}
-        className={emptyFields.includes("content") ? "error" : ""}
-      />
-
       {/* submit button */}
-      <button>Add Blog</button>
+      <button className="bg-[#E2B8AC] hover:bg-[#e0bbb0] text-white  my-5 py-2 px-4 rounded-full hover:drop-shadow-md duration-500 hover:scale-105">
+        Add Blog
+      </button>
       {error && <div className="error">{error}</div>}
 
       {/* filestack */}
