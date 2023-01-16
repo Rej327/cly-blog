194f5597bbd0418c2f32d6a795ad598ff29cb884
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-01-16 11:10:17 +0800

    Remove header and profile component in home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Header from "../components/Header";
 import MostPopularPost from "../components/MostPopularPost";
-import Profile from "../components/Profile";
 
 const Home = () => {
   return (
     <div>
-      <Header />
       <MostPopularPost />
-      <Profile />
     </div>
   );
 };
