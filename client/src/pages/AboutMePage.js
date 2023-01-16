71c9ebc04a679327cf49ef39b8775309edd11857
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-01-16 11:09:04 +0800

    Add about me page

--- client/src/pages/AboutMePage.js
+++ client/src/pages/AboutMePage.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { profile } from "../assets";
 
 const AboutMePage = () => {
   return (
-    <div>
-      <div>
-        <p>INTRODUCTION</p>
-        <h1>Welcome to Cly Blog</h1>
+    <div className="max-w-[900px] mx-auto mt-2">
+      <div className="text-center p-2 mt-32">
+        <p className="text-[10px] text-stone-400 font-bold tracking-widest">
+          INTRODUCTION
+        </p>
+        <h1 className="font-tenor text-3xl py-5">Welcome to Cly Blog</h1>
       </div>
       <div>
-        <p>
+        <p className="text-stone-500 text-center">
           Far far away, behind the word mountains, far from the countries
           Vokalia and Consonantia, there live the blind texts. Separated they
           live in Bookmarksgrove right at the coast of the Semantics, a large
@@ -18,13 +21,13 @@ const AboutMePage = () => {
           country, in which roasted parts of sentences fly into your mouth.
         </p>
       </div>
-      <div>
-        <div>
-          <img src="image.jpg" alt="/" />
+      <div className="grid grid-cols-3 mt-28">
+        <div className="max-w-[250px] my-auto mx-auto">
+          <img src={profile} alt="/" />
         </div>
-        <div>
-          <h1>Carolyn Ollarte</h1>
-          <p>
+        <div className="col-span-2">
+          <h1 className="font-tenor text-2xl">Carolyn Ollarte</h1>
+          <p className="text-stone-500 py-4">
             A small river named Duden flows by their place and supplies it with
             the necessary regelialia. Far far away, behind the word mountains,
             far from the countries Vokalia and Consonantia, there live the blind
@@ -34,7 +37,7 @@ const AboutMePage = () => {
             It is a paradisematic country, in which roasted parts of sentences
             fly into your mouth.
           </p>
-          <ul>
+          <ul className="text-stone-400 list-disc pl-10">
             <li>Far far away, behind the word mountains</li>
             <li>A small river named Duden flows by their place and supplies</li>
             <li>
@@ -42,9 +45,6 @@ const AboutMePage = () => {
               fly into your mouth.
             </li>
           </ul>
-          <div>
-            <SocialIcon />
-          </div>
         </div>
       </div>
     </div>
