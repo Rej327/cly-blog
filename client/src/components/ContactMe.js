99fb45047c5d4060cce2885d8a97f7c9f9f0dea6
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-01-19 12:03:35 +0800

    ContactMe component working using EMAILJS

--- client/src/components/ContactMe.js
+++ client/src/components/ContactMe.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const ContactMe = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_40mptef",
+        "template_54mqla4",
+        e.target,
+        "FSbjMbFNlJOLGuiuP"
+      )
+
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <div className="max-w-[1100px] mx-auto my-28 flex justify-center">
       <div className="max-w-[320px]">
@@ -14,33 +39,41 @@ const ContactMe = () => {
         <p className="text-gray-600 pb-3 flex text-sm">My instagram</p>
       </div>
       <div className="h-auto w-auto ml-5 ">
-        <div className="flex gap-8 w-max ">
+        <h1 className="font-tenor text-center text-2xl py-5 tracking-wider">
+          Get In Touch
+        </h1>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="flex gap-2 w-max ">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              required
+              className="h-11 w-[300px] p-2 rounded-md outline-[#E2B8AC]"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              required
+              className="h-11 w-[300px] p-2 rounded-md outline-[#E2B8AC]"
+            />
+          </div>
+          <div className="h-auto w-full  mt-2">
+            <textarea
+              type="text"
+              name="message"
+              placeholder="Message"
+              required
+              className="h-32 p-2 rounded-md w-full outline-[#E2B8AC]"
+            />
+          </div>
           <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            required
-            className="h-8 w-[300px] p-2 rounded-md"
+            type="submit"
+            value="Send"
+            className="bg-[#E2B8AC] hover:bg-[#e0bbb0] text-white mt-2 py-2 px-4 rounded-full hover:drop-shadow-md duration-500 w-full cursor-pointer"
           />
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            required
-            className="h-8 w-[300px] p-2 rounded-md"
-          />
-        </div>
-        <div className="h-auto w-full  mt-2">
-          <textarea
-            type="text"
-            placeholder="Message"
-            required
-            className="h-32 p-2 rounded-md w-full"
-          />
-        </div>
-        <button className="bg-[#E2B8AC] hover:bg-[#e0bbb0] text-white mt-2 py-2 px-4 rounded-full hover:drop-shadow-md duration-500 w-full">
-          Submit
-        </button>
+        </form>
       </div>
     </div>
   );
