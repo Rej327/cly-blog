d473a9fc1c938fe56b3f8c5c9a595ce623644d31
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-01-24 15:07:10 +0800

    Adding mobile responsive design of profile components

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -5,7 +5,7 @@ import Newsletter from "./Newsletter";
 
 const Profile = () => {
   return (
-    <div className="grid grid-cols-3 gap-8 px-28">
+    <div className="px-4 flex gap-4 mt-[-60px] flex-col-reverse sm:mt-auto sm:grid sm:grid-cols-3 sm:gap-8 sm:px-28">
       <AboutMe />
       <LatestBlog />
       <Newsletter />
