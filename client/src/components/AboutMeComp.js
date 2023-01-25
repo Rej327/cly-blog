bcd567b8cc357c686c780a8a8a5d71c8fd4c484f
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-01-25 11:37:53 +0800

    Responsive design tablet and phone in about content

--- client/src/components/AboutMeComp.js
+++ client/src/components/AboutMeComp.js
@@ -3,7 +3,7 @@ import { profile } from "../assets";
 
 const AboutMeComp = () => {
   return (
-    <div className="max-w-[900px] mx-auto mt-2">
+    <div className="md:max-w-[900px] mx-auto md:mt-2 mt-[600px]">
       <div className="text-center p-2 mt-32">
         <p className="text-[10px] text-stone-400 font-bold tracking-widest">
           INTRODUCTION
@@ -11,7 +11,7 @@ const AboutMeComp = () => {
         <h1 className="font-tenor text-3xl py-5">Welcome to Cly Blog</h1>
       </div>
       <div>
-        <p className="text-stone-500 text-center">
+        <p className="text-stone-500 text-center px-4">
           Far far away, behind the word mountains, far from the countries
           Vokalia and Consonantia, there live the blind texts. Separated they
           live in Bookmarksgrove right at the coast of the Semantics, a large
@@ -20,13 +20,13 @@ const AboutMeComp = () => {
           country, in which roasted parts of sentences fly into your mouth.
         </p>
       </div>
-      <div className="grid grid-cols-3 mt-28">
+      <div className="md:grid md:grid-cols-3 md:mt-28 p-4 ">
         <div className="max-w-[250px] my-auto mx-auto">
           <img src={profile} alt="/" />
         </div>
         <div className="col-span-2">
-          <h1 className="font-tenor text-2xl">Carolyn Ollarte</h1>
-          <p className="text-stone-500 py-4">
+          <h1 className="font-tenor text-2xl pt-4">Carolyn Ollarte</h1>
+          <p className="text-stone-500 py-4 px-4">
             A small river named Duden flows by their place and supplies it with
             the necessary regelialia. Far far away, behind the word mountains,
             far from the countries Vokalia and Consonantia, there live the blind
