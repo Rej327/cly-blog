224272dacedae1bd65d56dce536ed762c2352ff3
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-01-27 15:11:08 +0800

    Back

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for request
-    app.listen("https://cly-blog.onrender.com", () => {
+    app.listen(process.env.PORT, () => {
       console.log("connected to db and listening on port", process.env.PORT);
     });
   })
